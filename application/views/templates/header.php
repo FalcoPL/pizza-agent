29e294494bcbe7008be6e5d31b2092d6820d13a4
FalcoPL <thejacol2016@outlook.com>
2017-07-15 11:05:53 +0200

    0.0.3
    CONFIG
    add:
       bootstrap
    create:
       views:
          templates:
             header.php
             footer.php

---
+++ application/views/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Pizza Agent</title>
+	
+		<!-- Latest compiled and minified CSS & JS -->
+		<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<script src="https://code.jquery.com/jquery.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		
+		<link rel="stylesheet" href="<?php echo base_url('assets/css/styles.css'); ?>">
+	</head>
+	<body>
+		<div class="container-fluid">
