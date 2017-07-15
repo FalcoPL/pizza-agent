04c3b9767113c4cea56e67dd7c37fde68f9d98f5
FalcoPL <thejacol2016@outlook.com>
2017-07-15 17:59:54 +0200

    0.0.7
    created models

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '192.168.101.142',
-	'username' => 'thejacol_pizza',
-	'password' => '20170715',
-	'database' => 'thejacol_pizza-agent',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'pizza-agent',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
---
+++ application/models/product_model.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	* 
+	*/
+	class product_model extends CI_Model
+	{
+		
+		function __construct()
+		{
+			# code...
+		}
+	}
