32606fc24ce782f70e92c14e8e8ece091eb1f074
FalcoPL <thejacol2016@outlook.com>
2017-07-17 18:04:33 +0200

    0.1.8
    change DB to external

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'external';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -79,6 +79,28 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => '',
 	'database' => 'pizza-agent',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['external'] = array(
+	'dsn'	=> '',
+	'hostname' => 'mn22.webd.pl',
+	'username' => 'thejacol_pizza',
+	'password' => '20170717',
+	'database' => 'thejacol_pizza-agent',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
